6b2c1e4d1efa7bc803fb1cafc8e142b5e367bd45
hjunieee <hinara12@inu.ac.kr>
2025-06-25 21:22:05 +0900

    fix: React가 인식하지 못하는 props (isToday 등)를 DOM에 전달하던 문제 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,16 +1,39 @@
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
+  const navigate = useNavigate();
   return (
     <LoginPageWrapper>
-로그인 페이지입니다.
+      <div>
+        <h1>로그인</h1>
+
+        <h3>아이디</h3>
+        <input />
+        <h3>비밀번호</h3>
+        <input />
+      </div>
+
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        로그인
+      </button>
     </LoginPageWrapper>
   );
 }
 
 const LoginPageWrapper = styled.div`
-  padding: 0 32px;
-  height: calc(1024px - 100px);
-  padding-bottom: 100px;
+  padding: 20px;
   display: flex;
+  flex-direction: column;
+  gap: 20px;
+  box-sizing: border-box;
+
+  width: 100vw;
+  height: 100vh;
+  overflow-y: auto;
+  justify-content: space-between;
 `;
