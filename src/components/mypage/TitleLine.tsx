578d160b1d25321bdc26e7d2f09ad5a8afc10664
hjunieee <hinara12@inu.ac.kr>
2025-06-25 20:50:11 +0900

    feat: 마이페이지 퍼블리싱

---
+++ src/components/mypage/TitleLine.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+interface TitleLineProps {
+  title: string;
+}
+
+const TitleLine = ({ title }: TitleLineProps) => {
+  return (
+    <TitleLineWrapper>
+      <div className="title">{title}</div>
+    </TitleLineWrapper>
+  );
+};
+
+export default TitleLine;
+
+const TitleLineWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 8px;
+
+  width: 100%;
+  height: fit-content;
+
+  .title {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+    /* 상자 높이와 동일 또는 150% */
+    display: flex;
+    align-items: center;
+    letter-spacing: 0.38px;
+
+    color: #1c1c1e;
+  }
+`;
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,3 +1,75 @@
-export default function MyPage() {
-  return <div>마이페이지입니다.</div>;
-}
+import styled from "styled-components";
+import MyInfoArea from "../components/mypage/MyInfoArea.tsx";
+import MenuGroup from "../components/mypage/MenuGroup.tsx";
+import React from "react";
+
+const menuGroups = [
+  {
+    title: "내 계정",
+    menus: [
+      { label: "프로필 수정", path: "/profile/edit" },
+      { label: "사전 체크리스트 수정", path: "/checklist/edit" },
+    ],
+  },
+  {
+    title: "커뮤니티",
+    menus: [
+      { label: "내 게시글 보기", path: "/community/posts" },
+      { label: "스크랩한 글 보기", path: "/community/liked" },
+      { label: "좋아요한 글 보기", path: "/community/liked" },
+    ],
+  },
+  {
+    title: "룸메이트",
+    menus: [
+      { label: "룸메이트 등록하기", path: "/roommate/apply" },
+      { label: "룸메이트 해제하기", path: "/roommate/result" },
+    ],
+  },
+  {
+    title: undefined,
+    menus: [
+      { label: "1:1 문의", path: "/roommate/apply" },
+      { label: "서비스 정보", path: "/roommate/result" },
+    ],
+  },
+];
+
+const MyPage = () => {
+  return (
+    <MyPageWrapper>
+      <MyInfoArea />
+      <Divider />
+      {menuGroups.map((group, idx) => (
+        <React.Fragment key={idx}>
+          <MenuGroup title={group.title} menus={group.menus} />
+          {idx < menuGroups.length - 1 && <Divider />}
+        </React.Fragment>
+      ))}
+    </MyPageWrapper>
+  );
+};
+
+export default MyPage;
+
+const MyPageWrapper = styled.div`
+  padding: 90px 20px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  box-sizing: border-box;
+
+  width: 100%;
+  height: 100%;
+
+  overflow-y: auto;
+
+  //background: #fafafa;
+`;
+
+const Divider = styled.div`
+  height: 1px;
+  background-color: #e0e0e0; /* 연한 회색 */
+  width: 100%;
+`;
