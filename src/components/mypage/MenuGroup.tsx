695a763852828224d991d2d81633ee70e45760de
﻿hjunieee <hinara12@inu.ac.kr>
2025-06-25 20:50:11 +0900

    feat: 마이페이지 퍼블리싱

---
+++ src/components/mypage/MenuGroup.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import TitleLine from "./TitleLine.tsx";
+import { useNavigate } from "react-router-dom";
+
+interface MenuItem {
+  label: string;
+  path: string;
+}
+
+interface MenuGroupProps {
+  title?: string;
+  menus: MenuItem[];
+}
+
+const MenuGroup = ({ title, menus }: MenuGroupProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <MenuGroupWrapper>
+      {title && <TitleLine title={title} />}
+      {menus.map((menu, index) => (
+        <MenuItem key={index} onClick={() => navigate(menu.path)}>
+          {menu.label}
+        </MenuItem>
+      ))}
+    </MenuGroupWrapper>
+  );
+};
+
+export default MenuGroup;
+
+const MenuGroupWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: fit-content;
+
+  gap: 20px;
+`;
+const MenuItem = styled.div`
+  width: 100%;
+  height: fit-content;
+`;
---
+++ src/components/mypage/MyInfoArea.tsx
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import profileimg from "../../assets/profileimg.svg";
+const MyInfoArea = () => {
+  return (
+    <MyInfoAreaWrapper>
+      <LeftArea>
+        <img src={profileimg} />
+        <div className="description">
+          <div className="name">익명</div>
+          <div className="college">글로벌정경대</div>
+        </div>
+      </LeftArea>
+      <Penalty>벌점 3점</Penalty>
+    </MyInfoAreaWrapper>
+  );
+};
+
+export default MyInfoArea;
+
+const MyInfoAreaWrapper = styled.div`
+  width: 100%;
+  height: fit-content;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LeftArea = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  min-width: fit-content;
+  height: fit-content;
+  gap: 8px;
+
+  .description {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    font-style: normal;
+    font-size: 16px;
+    line-height: 24px;
+    align-items: start;
+    justify-content: center;
+    letter-spacing: 0.38px;
+
+    color: #1c1c1e;
+    .name {
+      font-weight: 700;
+    }
+    .college {
+      font-weight: 400;
+    }
+  }
+`;
+
+const Penalty = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 16px;
+  gap: 5px;
+
+  min-width: fit-content;
+  height: fit-content;
+
+  background: #0a84ff;
+  border-radius: 23px;
+
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+
+  letter-spacing: 0.38px;
+
+  color: #f4f4f4;
+`;
