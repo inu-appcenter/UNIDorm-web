bfe15690a8121d4b1312df3f6034180f5395253f
realllyoujin <reallllyoujin@gmail.com>
2025-07-07 17:50:28 +0900

    feat: 공동구매페이지 퍼블리싱

--- src/components/GroupPurchase/GroupPurchaseItem.tsx
+++ src/components/GroupPurchase/GroupPurchaseItem.tsx
@@ -1,11 +1,20 @@
 import styled from "styled-components";
+import human from "../../assets/chat/human.svg";
 
 const GroupPurchaseItem = () => {
   return (
     <GroupPurchaseItemWrapper>
       <ItemImage />
       <TitleLine>휴지 공동구매 인원 구해요</TitleLine>
-      <AdditionalLine>D-1 | 3/4</AdditionalLine>
+      <Price>24,000원</Price>
+      <AdditionalLine>
+        <span className="dDay">D-1</span>
+        <span className="divider">|</span>
+        <span className="people">
+          <img src={human} alt="인원 아이콘" />
+          3/4
+        </span>
+      </AdditionalLine>
     </GroupPurchaseItemWrapper>
   );
 };
@@ -19,7 +28,6 @@ const GroupPurchaseItemWrapper = styled.div`
   justify-content: center;
   height: fit-content;
   gap: 10px;
-
   width: 150px;
 `;
 
@@ -29,26 +37,52 @@ const ItemImage = styled.div`
   background: rgba(0, 0, 0, 0.2);
   border-radius: 10px;
 `;
+
 const TitleLine = styled.div`
   width: 100%;
   font-style: normal;
   font-weight: 500;
   font-size: 17px;
   line-height: 21px;
-  /* 또는 124% */
   letter-spacing: -0.165px;
-
   color: #000000;
 `;
+
+const Price = styled.div`
+  width: 100%;
+  font-size: 16px;
+  font-weight: bold;
+  color: #000;
+`;
+
 const AdditionalLine = styled.div`
   width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 6px;
   font-style: normal;
   font-weight: 500;
   font-size: 14px;
   line-height: 21px;
-  /* 상자 높이와 동일 또는 150% */
   letter-spacing: -0.165px;
+  color: #666;
 
-  /* 서브2 */
-  color: #f97171;
+  .dDay {
+    color: #f97171;
+  }
+
+  .divider {
+    color: #aaa;
+  }
+
+  .people {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+
+    img {
+      width: 14px;
+      height: 14px;
+    }
+  }
 `;
