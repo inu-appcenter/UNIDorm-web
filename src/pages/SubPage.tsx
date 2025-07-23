2a00e0fb7071a31c3f686b86ecbc4f18e2953fdd
hjunieee <hinara12@inu.ac.kr>
2025-07-23 22:49:52 +0900

    접속기기에 다른 패딩 조정 시도7

--- src/pages/SubPage.tsx
+++ src/pages/SubPage.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
+import { getMobilePlatform } from "../utils/getMobilePlatform"; // 경로는 맞게 수정해주세요
+
+const platform = getMobilePlatform();
 
 export default function SubPage() {
   return (
-    <SubPageWrapper>
+    <SubPageWrapper $isIOS={platform === "ios"}>
       {/*<Header hasBack={true} />*/}
       <Outlet />
       {/*<BottomBar />*/}
@@ -11,7 +14,13 @@ export default function SubPage() {
   );
 }
 
-const SubPageWrapper = styled.div`
+const SubPageWrapper = styled.div<{ $isIOS: boolean }>`
   width: 100vw;
   height: 100vh;
+
+  ${({ $isIOS }) =>
+    $isIOS &&
+    `
+      padding-top: calc(env(safe-area-inset-top, 0px) * 0.5);
+    `}
 `;
