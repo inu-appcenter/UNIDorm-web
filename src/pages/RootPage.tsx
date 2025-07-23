16b0d3ec4fe84cf8c0c9a6b4de32b8349ee6ff94
hjunieee <hinara12@inu.ac.kr>
2025-07-23 22:44:21 +0900

    접속기기에 다른 패딩 조정 시도5

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,10 +1,20 @@
 import BottomBar from "../components/common/BottomBar.tsx";
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
+import { getMobilePlatform } from "../utils/getMobilePlatform.ts";
+import { useEffect, useState } from "react";
 
 export default function RootPage() {
+  const [platform, setPlatform] = useState<"ios" | "android" | "other">(
+    "other",
+  );
+
+  useEffect(() => {
+    setPlatform(getMobilePlatform());
+  }, []);
+
   return (
-    <RootPageWrapper>
+    <RootPageWrapper $platform={platform}>
       {/*<Header />*/}
       <Outlet />
       <BottomBar />
@@ -12,7 +22,12 @@ export default function RootPage() {
   );
 }
 
-const RootPageWrapper = styled.div`
+const RootPageWrapper = styled.div<{ $platform: string }>`
   width: 100vw;
   height: 100vh;
+
+  box-sizing: border-box;
+
+  ${({ $platform }) =>
+    $platform === "ios" ? `padding-top: env(safe-area-inset-top, 0px);` : ""}
 `;
