df97126e5751e7685c5c1a1ae50d3ae0590f691e
﻿hjunieee <hinara12@inu.ac.kr>
2025-07-25 22:27:33 +0900

    Feat: 첫 접속 사용자를 위한 온보딩 페이지

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// https://vite.dev/config/
+import path from "path";
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    },
+    define: {
+        global: "window",
+    },
 });
