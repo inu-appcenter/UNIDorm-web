cef62bfb071b623a6496b7a38db45f294c55a22e
hjunieee <hinara12@inu.ac.kr>
2025-08-08 22:08:18 +0900

    feat: 배포 환경에서는 콘솔 로그 안뜨게

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,13 @@ export default defineConfig({
   define: {
     global: "window",
   },
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 });
