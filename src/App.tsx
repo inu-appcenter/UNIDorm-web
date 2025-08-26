983bb800fcc9bc02e312ac9b99d599f992f3196a
﻿hjunieee <hinara12@inu.ac.kr>
2025-08-26 17:08:29 +0900

    fix: fcm토큰 발급 비활성화-앱에서 처리 예정

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ import CalendarAdminPage from "./pages/Admin/CalendarAdminPage";
 import CalendarPage from "./pages/CalendarPage";
 
 import "./init";
-import { useFcmToken } from "./hooks/useFcmToken";
+// import { useFcmToken } from "./hooks/useFcmToken";
 import { RoomMateProvider } from "./stores/RoomMateContext.tsx";
 import { AnnouncementProvider } from "./stores/AnnouncementContext.tsx";
 import { TipProvider } from "./stores/TipContext.tsx";
@@ -66,7 +66,7 @@ import { ErrorBoundary } from "./ErrorBoundary.tsx";
 
 function App() {
   console.log("현재 MODE:", import.meta.env.MODE);
-  useFcmToken();
+  // useFcmToken();  //웹뷰환경에서 이 로직이 돌면 오류발생하므로, 앱에서 알림 처리해줘야됨
 
   if (import.meta.env.MODE === "production") {
     console.log = () => {};
