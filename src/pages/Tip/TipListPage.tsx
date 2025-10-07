f8c31684cefdd5ab8a163f95a01942ae9de7dcb3
hjunieee <hinara12@inu.ac.kr>
2025-10-08 01:54:07 +0900

    refactor: 공지사항 작성/수정페이지 헤더 통일

--- src/pages/Tip/TipListPage.tsx
+++ src/pages/Tip/TipListPage.tsx
@@ -35,7 +35,7 @@ export default function TipListPage() {
 
   return (
     <TipPageWrapper>
-      <Header title="기숙사 꿀팁" hasBack={true} showAlarm={true} />
+      <Header title="기숙사 꿀팁" hasBack={true} />
 
       <TitleContentArea
         title="기숙사 꿀팁"
