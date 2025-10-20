b6c4659e76d2f911af89124397035829c7f0cd8d
hjunieee <korea024504@gmail.com>
2025-10-20 11:36:50 +0900

    fix: 로그인페이지 레이아웃 문제

--- src/pages/OutPage.tsx
+++ src/pages/OutPage.tsx
@@ -4,17 +4,25 @@ import styled from "styled-components";
 export default function OutPage() {
   return (
     <OutPageWrapper>
-      <Outlet />
+      <StyledOutletContainer>
+        <Outlet />
+      </StyledOutletContainer>
     </OutPageWrapper>
   );
 }
 
 const OutPageWrapper = styled.div`
   width: 100%;
-  min-height: 100%; /* 뷰포트 전체 높이를 기준으로 설정 */
+  min-height: 100vh; /* 뷰포트 전체 높이를 기준으로 설정 */
   height: fit-content;
   box-sizing: border-box;
   background: #fafafa;
   display: flex;
+  flex-direction: column;
+`;
+
+const StyledOutletContainer = styled.div`
+  flex-grow: 1; /* 남은 공간을 모두 차지하도록 설정 */
+  display: flex; /* 내부 콘텐츠 정렬을 위해 추가 */
   flex-direction: column;
 `;
