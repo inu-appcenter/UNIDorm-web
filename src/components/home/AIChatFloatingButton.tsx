a1db2d2ae347a1805454245dc479e11323de0a3a
hjunieee <hinara12@inu.ac.kr>
2026-07-09 16:54:05 +0900

    chore: 챗불이 mode파라미터 제거

--- src/components/home/AIChatFloatingButton.tsx
+++ src/components/home/AIChatFloatingButton.tsx
@@ -117,11 +117,7 @@ const AIChatFloatingButton = () => {
                     import.meta.env.VITE_API_SUBDOMAIN === "unidorm-server"
                       ? import.meta.env.VITE_INUCHAT_URL // 운영용 URL
                       : import.meta.env.VITE_INUCHAT_DEV_URL // 개발용 URL
-                  }/?token=${accessToken || ""}&mode=${
-                    import.meta.env.VITE_API_SUBDOMAIN === "unidorm-server"
-                      ? "prod"
-                      : "dev"
-                  }`}
+                  }/?token=${accessToken || ""}`}
                   title="AI Chat"
                   width="100%"
                   height="100%"
